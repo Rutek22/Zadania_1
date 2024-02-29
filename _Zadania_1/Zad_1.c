3ddc8c1e9cd5c99b27c7b93f0252e7625e681f7e
Rutek22 <krystian.rutkowski17@gmail.com>
2024-02-29 19:40:33 +0100

    Naprawa problemow z kompilacja

--- _Zadania_1/Zad_1.c
+++ _Zadania_1/Zad_1.c
@@ -1,11 +1,10 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 int main()
 {
     char name[50];
     printf("Wprowadz swoje imie: ");
-    scanf_s("%s", name, (unsigned)_countof(name));
+    scanf("%s", name);
     printf("Witaj %s", name);
     return 0;
 }
